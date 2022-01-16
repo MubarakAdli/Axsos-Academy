b8400251b1c7d11749b8390f118b554cedf1f1d5
MubarakAdli <mubarakadli_cr@hotmail.com>
2022-01-16 19:14:13 +0200

    initiate

---
+++ WEB Fundamentals/JavaScript/Dojo Pizzeria/Dojo Pizzeria.js
@@ -0,0 +1,20 @@
+function pizzaOven(crustType,sauceType,cheeses,toppings){
+var pizza={};
+pizza.crustType=  "deep dish";
+pizza.sauceType= "traditional";
+pizza.cheeses= "cmozzarella";
+pizza.toppings= ["pepperoni", "sausage"]
+return pizza;
+}
+
+var pizza1=pizzaOven("deep dish","traditional","mozzarella",["pepperoni", "sausage"]);
+console.log(pizza1);
+
+var pizza2=pizzaOven("hand tossed","marinara",["mozzarella", "feta"],["mushrooms", "olives", "onions"]);
+console.log(pizza2);
+
+var mypizza=pizzaOven("thick","traditional","feta",["pepperoni","olives", "sausage"]);
+console.log(mupizaa);
+
+var mypizza2=pizzaOven("tthin","marinara","ozzarella",["pepperoni","olives"]);
+console.log(mupizaa2);
