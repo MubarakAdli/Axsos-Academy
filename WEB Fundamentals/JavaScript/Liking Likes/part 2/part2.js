9c2e2a135da799724811d545acdadcf95b4fee23
MubarakAdli <mubarakadli_cr@hotmail.com>
2022-01-16 18:15:04 +0200

    initite

---
+++ WEB Fundamentals/JavaScript/Liking Likes/part 2/part2.js
@@ -0,0 +1,25 @@
+var count=9;
+var sec=12;
+var thi=9;
+
+
+
+
+function add(){
+var countele=document.querySelector("#inc");
+count++;
+countele.innerText= count+ "like(s)"
+}
+
+
+function plus(){
+    var countele=document.querySelector("#second");
+    sec++;
+    countele.innerText= sec+ "like(s)"
+    }
+
+    function incr(){
+        var countele=document.querySelector("#third");
+        thi++;
+        countele.innerText= thi +"like(s)"
+        }
