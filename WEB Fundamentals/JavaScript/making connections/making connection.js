d11348b1fd104f7cf7c5a6be7f0f4a44e1d8aff0
MubarakAdli <mubarakadli_cr@hotmail.com>
2022-01-17 19:15:10 +0200

    intiate

---
+++ WEB Fundamentals/JavaScript/making connections/making connection.js
@@ -0,0 +1,22 @@
+
+var element=document.querySelector(".comp1");
+var sub=document.querySelector(".comp2");
+var ko=document.querySelector("h1");
+var count=500;
+
+// remove ToDD E from Connection Requests
+function remove(){
+    element.remove();
+}
+
+// remove Phil K from Connection Requests
+function del(){
+    sub.remove();
+}
+
+// change edit profile to Mubarak
+function sli(){
+    ko.innerText = "Mubarak";
+}
+
+
