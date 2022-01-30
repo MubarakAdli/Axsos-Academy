b802db27b23e1e1b293e23989522d18c54ee91fc
MubarakAdli <mubarakadli_cr@hotmail.com>
2022-01-30 11:53:19 +0200

    initiate

---
+++ python_stack/_python/hello_world.py
@@ -0,0 +1,17 @@
+print("Hello World")
+x="Mubarak"
+print("Hello",x)
+print("Hello"+" "+x)
+
+y=30
+print("Hello",y)
+print("Hello "+str(y))
+
+food="shawerma"
+food_2="falafel"
+print("I love eat {} and {} ".format (food,food_2))
+print(f"I love eat {food} and {food_2} ")
+
+s=x.upper()
+print(s)
+print(x.split("b"))
