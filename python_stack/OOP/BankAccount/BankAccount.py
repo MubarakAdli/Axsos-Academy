f7273fd60affc081347ef4fcce5451dc39a016d2
MubarakAdli <mubarakadli_cr@hotmail.com>
2022-02-01 01:39:22 +0200

    initiate

---
+++ python_stack/OOP/BankAccount/BankAccount.py
@@ -0,0 +1,33 @@
+class bankaccount:
+    def __init__(self,interest_rate,balance):
+        self.interest_rate=interest_rate
+        self.balance=balance
+    def deposit(self,amount):
+        self.balance =self.balance+amount
+        return self.balance
+    def withdraw(self,amount):
+        self.balance =self.balance-amount   
+        return self.balance
+    def display_account_info(self):
+        print(self.balance)
+    def yield_interest(self):
+        self.balance=self.balance*(self.interest_rate + 1)
+        return self.balance
+
+account_1=bankaccount(0.02,500)
+account_2=bankaccount(0.03,700)
+
+account_1.deposit(300)
+account_1.deposit(200)
+account_1.deposit(200)
+account_1.withdraw(400)
+account_1.yield_interest()
+account_1.display_account_info()
+account_2.deposit(100)
+account_2.deposit(700)
+account_2.withdraw(200)
+account_2.withdraw(200)
+account_2.withdraw(100)
+account_2.withdraw(200)
+account_2.yield_interest()
+account_2.display_account_info()
