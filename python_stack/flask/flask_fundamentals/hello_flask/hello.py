ca8e79c99cb7ebce024f2df47d8c21fce3516b5d
MubarakAdli <mubarakadli_cr@hotmail.com>
2022-02-02 13:14:07 +0200

    initiate

---
+++ python_stack/flask/flask_fundamentals/hello_flask/hello.py
@@ -0,0 +1,23 @@
+from ast import Num
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')          
+def hello():
+    print("in the hello function")
+    return ("Hello World!")
+@app.route('/dojo')          
+def hello_1():
+    print("in the hello function")
+    return ("Dojo")
+@app.route('/<name>') 
+def hello_person(name):
+    print("in the hello_person function")
+    print(name)
+    return f"Hi  {name}"
+@app.route('/repeat/<num>/<name>') 
+def hello_number(name,num):
+    return f'{name}' * int(num)
+if __name__=="__main__":     
+    app.run(debug=True)
+
