6dd01cc70b6f167c0c2bb5bf7422abd74112fd1d
MubarakAdli <mubarakadli_cr@hotmail.com>
2022-03-05 15:36:42 +0200

    commit

---
+++ Java/Java Fundamentals/Java First Program/First.java
@@ -0,0 +1,7 @@
+public class First { 
+    public static void main( String [] args) { 
+        System.out.println("My Name is Mubarak");
+        System.out.println("My age is 30");
+        System.out.println("I am from Ramallah");
+   }  
+}
