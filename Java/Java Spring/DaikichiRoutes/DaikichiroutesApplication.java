1d1862ea61888c1be90248bc2e98eb8fb2491e05
MubarakAdli <mubarakadli_cr@hotmail.com>
2022-03-11 14:23:25 +0200

    commit

---
+++ Java/Java Spring/DaikichiRoutes/DaikichiroutesApplication.java
@@ -0,0 +1,13 @@
+package com.axsos.DaikichiRoutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiroutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiroutesApplication.class, args);
+	}
+
+}
