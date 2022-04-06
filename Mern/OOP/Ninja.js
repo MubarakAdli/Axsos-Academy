22b4c100c1b1e739031f3480f7f15e53c324d2e7
MubarakAdli <mubarakadli_cr@hotmail.com>
2022-04-06 21:18:26 +0300

    Ninja

---
+++ Mern/OOP/Ninja.js
@@ -0,0 +1,28 @@
+// parent Vehicle class
+class Ninja {
+    constructor(manufacturer, model, color) {
+        this.name = name;
+        this.hhealth = health;
+        this.speed = 3;
+        this.strength = 3; 
+    }
+
+    sayName() {
+        console.log(this.name);
+    }
+
+    showStats() {
+        console.log(`Name: ${this.name}`);
+        console.log(`Strength: ${this.strength}`);
+        console.log(`Speed: ${this.speed}`);
+        console.log(`Health: ${this.health}`);
+    }
+    drinkSake() {
+        this.health += 10;
+    }
+
+}
+
+const ninja1 = new Ninja("Hyabusa");
+    ninja1.sayName();
+    ninja1.showStats();
